658e89771deab35e0bdff65c1fd97e127b90b50f
marija0708 <marija.marijanovic90@gmail.com>
2026-03-05 07:06:09 +0100

    Add Container component and update layout in Header and Footer; include Instagram and TikTok icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import AppRouter from './router/AppRouter';
 import Header from './components/layout/Header';   
 import Footer from './components/layout/Footer';
+import Container from './components/layout/Container';
 
 function App() {
   return (
     <>
+      <Container>
       <Header />
       <main className="pt-24">
         <AppRouter />
-      </main>
-      <Footer />  
+        </main>
+     </Container>
+    <Footer />  
     </>
   )
 }
