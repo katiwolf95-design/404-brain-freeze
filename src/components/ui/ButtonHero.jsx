b358cfd372f3254560d460f94ed0c2731dea1e1a
Anne Sasse <annes.94@outlook.de>
2026-03-07 09:51:16 +0100

    Refactor IcePreview to use Button component and fix ButtonHero Hover Typo

--- src/components/ui/ButtonHero.jsx
+++ src/components/ui/ButtonHero.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function ButtonHero({ children, to, onClick, className }) {
 
-  const btnStyle = "bg-[#7EA705] text-white text-lg tracking-wide px-10 py-3 rounded-full shadow-[4px_4px_4px_rgba(0,0,0,0.25)] hover:scale-105 hover:bg-[#6B8F04 transition-transform"
+  const btnStyle = "bg-[#7EA705] text-white text-lg tracking-wide px-10 py-3 rounded-full shadow-[4px_4px_4px_rgba(0,0,0,0.25)] hover:scale-105 hover:bg-[#6B8F04] transition-transform"
 
   if (to) {
     return (
